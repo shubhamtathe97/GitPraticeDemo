d725b7a23b6e785a155bc8cf5e667a229c50d658
shubhamtathe97 <shubhamtathe97@gmail.com>
2022-07-11 17:08:01 +0530

    added class 1 by shubham

---
+++ src/test/java/com/visionit/automation/pageobjects/DemoClass.java
@@ -0,0 +1,12 @@
+package com.visionit.automation.pageobjects;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("this is newly add class");
+		// TODO Auto-generated method stub
+
+	}
+
+}
